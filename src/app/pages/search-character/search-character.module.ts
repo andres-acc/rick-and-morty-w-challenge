d36d33d22164c49c33a1770a8e48ce4b74122f5a
andres-acc <elkin.alcaraz@accenture.com>
2023-03-16 15:26:40 -0500

    feat: Add main search page

--- src/app/pages/search-character/search-character.module.ts
+++ src/app/pages/search-character/search-character.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SearchCharacterComponent } from './search-character.component';
+import { BigInputModule } from 'src/app/components/big-input/big-input.module';
 import { SearchCharacterRoutingModule } from './search-character-routing.module';
 
 @NgModule({
   declarations: [SearchCharacterComponent],
-  imports: [CommonModule, SearchCharacterRoutingModule],
+  imports: [
+    CommonModule,
+    BigInputModule, 
+    SearchCharacterRoutingModule
+  ],
 })
 export class SearchCharacterModule {}
