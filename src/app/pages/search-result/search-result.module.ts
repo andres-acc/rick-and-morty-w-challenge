2d10566a4992b4f170add2ca1f8e3c46d129307d
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2023-03-23 19:01:34 -0500

    feat: add header to search result page

--- src/app/pages/search-result/search-result.module.ts
+++ src/app/pages/search-result/search-result.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SearchResultComponent } from './search-result.component';
 import { SearchResultRoutingModule } from './search-result-routing.module';
+import { BigInputModule } from '../../components/big-input/big-input.module';
 
 @NgModule({
   declarations: [SearchResultComponent],
-  imports: [CommonModule, SearchResultRoutingModule],
+  imports: [CommonModule, SearchResultRoutingModule, BigInputModule],
 })
 export class SearchResultModule {}
