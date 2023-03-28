d6b863bac80135a10cf7092a272bbdcf90f283ff
andres-acc <elkin.alcaraz@accenture.com>
2023-03-28 13:27:31 -0500

    feat: Improve conditionals and format code

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -5,7 +5,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent  {
+export class ButtonComponent {
   @Input() content: string = 'Female';
   @Input() badge: number = 0;
   @Output() action = new EventEmitter<void>();
