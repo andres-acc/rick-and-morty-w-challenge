1eca89ba43e539db328976b3bf9600bed872b3e1
Juan Miguel Henao Arias <henaoariasjm@gmail.com>
2023-03-28 19:43:22 -0500

    feat: render characters

--- src/app/pages/search-result/search-result.module.ts
+++ src/app/pages/search-result/search-result.module.ts
@@ -4,9 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SearchResultComponent } from './search-result.component';
 import { SearchResultRoutingModule } from './search-result-routing.module';
 import { BigInputModule } from '../../components/big-input/big-input.module';
+import { AccordionModule } from '../../components/accordion/accordion.module';
+import { CharacterCardModule } from '../../components/character-card/character-card.module';
+import { ApiService } from '../../services/api.service';
+import { PaginatorModule } from '../../components/paginator/paginator.module';
 
 @NgModule({
   declarations: [SearchResultComponent],
-  imports: [CommonModule, SearchResultRoutingModule, BigInputModule],
+  imports: [CommonModule, SearchResultRoutingModule, BigInputModule, AccordionModule, CharacterCardModule, PaginatorModule],
+  providers: [ApiService]
 })
 export class SearchResultModule {}
